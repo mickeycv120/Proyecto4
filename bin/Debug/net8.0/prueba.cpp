8df37524798367139e0180eaf14b4a293ed1babc
Efrain Coutiño <l22141208@queretaro.tecnm.mx>
2024-11-26 15:45:41 -0600

    Prueba

--- bin/Debug/net8.0/prueba.cpp
+++ bin/Debug/net8.0/prueba.cpp
@@ -5,52 +5,22 @@ using System.Threading.Tasks;
 
 static void Main(string[] args)
 {
-    char c;
-    int  a,b,c,d;
-    int  i,k;
+  int a, b = Console.ReadLine();
+  a = (3 + 5) * 8 - (10 - 2*b) / b; // 61
 
-    a = (3 + 5) * 8 - (10 - 4) / 2;
+  int c = 0;
+  c++;
+  c++;
+  c /= 2;
 
-    if (1 != 2)
-    {
-        Console.Write("Ingrese el valor de d = ");
-        d = Console.ReadLine();
-        if (d % 2 == 0)
-        {
-            for (i = 0; i < d; i++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
-            i = 0;
-            do
-            {
-                Console.Write("-");
-                i++;
-            } while (i < d);
-            i = 0;
-            Console.WriteLine();
-            while (i < d)
-            {
-                Console.Write("-");
-                i++;
-            }
-            for (i = d; i >= 0; i--)
-            {
-                Console.Write(" - ");
-            }
-            Console.WriteLine();
-            for (i = 0; i < d; i += 2)
-            {
-                for (j = 0; j <= i; j += 1)
-                {
-                    if (j % 2 == 0)
-                        Console.Write("+");
-                    else
-                        Console.Write("-");
-                }
-                Console.Write();
-            }
-        }
-    }
+  if(a + 9 == 70) {
+    Console.WriteLine("Es verdadero");
+    if(b == 2) {
+      Console.WriteLine("b es igual a 2");
+
+      if(b > 3) {
+        Console.WriteLine("b es mayor a 3");
+      }
+    }
+  }
 }
